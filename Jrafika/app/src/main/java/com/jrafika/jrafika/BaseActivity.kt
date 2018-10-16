f7c63a642db2f0bc3dc028d56f5ca7e386ef02eb
Jauhar Arifin <jauhararifin10@gmail.com>
2018-10-16 07:28:55 +0700

    add menu and task 1 activity

--- Jrafika/app/src/main/java/com/jrafika/jrafika/BaseActivity.kt
+++ Jrafika/app/src/main/java/com/jrafika/jrafika/BaseActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.navigation_layout.*
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 
@@ -20,28 +21,28 @@ abstract open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_layout)
+
         mToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(mToggle!!)
         mToggle!!.syncState()
         getSupportActionBar()!!.setDisplayHomeAsUpEnabled(true)
 
-        navigationView.setNavigationItemSelectedListener {
-            when (it.getItemId()) {
-                R.id.task1MenuOption-> {
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    true
-                }
-                else -> true
-            }
-        }
-
         val inflater: LayoutInflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val contentView: View = inflater.inflate(contentViewId, null, false)
-        drawerLayout.addView(contentView)
+        drawerLayout.addView(contentView, 0)
+
+        navigationView.setNavigationItemSelectedListener {
+            Log.d("wow", "menu selected " + it.itemId)
+            if (it.itemId == R.id.task1MenuOption) {
+                val intent = Intent(this.getApplicationContext(), Task1Activity::class.java)
+                startActivity(intent)
+            }
+            true
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        Log.d("wow", "option item selected " + item!!.itemId)
         if (mToggle!!.onOptionsItemSelected(item)) {
             return true
         }
---
+++ Jrafika/app/src/main/java/com/jrafika/jrafika/Task1Activity.kt
@@ -0,0 +1,8 @@
+package com.jrafika.jrafika
+
+class Task1Activity : BaseActivity() {
+
+    override val contentViewId: Int
+        get() = R.layout.activity_task1
+
+}
