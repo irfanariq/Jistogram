42795fef523b57ed0003cb61f995a0987cdb45a0
Jauhar Arifin <jauhararifin10@gmail.com>
2018-11-05 09:28:33 +0700

    add custom convolution

--- Jrafika/app/src/main/java/com/jrafika/jrafika/task/filters/customfilter/CustomFilterActivity.kt
+++ Jrafika/app/src/main/java/com/jrafika/jrafika/task/filters/customfilter/CustomFilterActivity.kt
@@ -7,6 +7,7 @@ import com.jrafika.jrafika.BaseActivity
 import com.jrafika.jrafika.ImageResultFragment
 import com.jrafika.jrafika.ImportImageFragment
 import com.jrafika.jrafika.R
+import com.jrafika.jrafika.processor.Convoluter
 import com.jrafika.jrafika.processor.ImageGrayscaler
 import com.jrafika.jrafika.task.histogram.ImageTask
 import kotlinx.android.synthetic.main.task_layout.*
@@ -23,24 +24,38 @@ class CustomFilterActivity: BaseActivity() {
 
         val importImageFragment = ImportImageFragment()
         val grayscaledImageFragment = ImageResultFragment()
+        val customFilterOptionFragment = CustomFilterOptionFragment()
         val resultImageFragment = ImageResultFragment()
         importImageFragment.imageImportedListener = {
             ImageTask(grayscaledImageFragment, ImageGrayscaler())
                     .execute(it)
+            grayscaledImageFragment.imageLoadedListener = {
+                val image = it
+                customFilterOptionFragment.proceedFunction = {
+                    val kernel = arrayOf(
+                            floatArrayOf(it[0], it[1], it[2]),
+                            floatArrayOf(it[3], it[4], it[5]),
+                            floatArrayOf(it[6], it[7], it[8])
+                    )
+                    ImageTask(resultImageFragment, Convoluter(kernel)).execute(image)
+                    taskViewPager.setCurrentItem(3)
+                }
+            }
             taskViewPager.setCurrentItem(2)
         }
 
-        taskViewPager.offscreenPageLimit = 3
+        taskViewPager.offscreenPageLimit = 4
         taskViewPager.adapter = object: FragmentStatePagerAdapter(supportFragmentManager) {
             override fun getItem(p0: Int): Fragment {
                 return arrayOf(
                         importImageFragment,
                         grayscaledImageFragment,
+                        customFilterOptionFragment,
                         resultImageFragment
                 )[p0]
             }
             override fun getCount(): Int {
-                return 3
+                return 4
             }
         }
         taskViewPager.setCurrentItem(0)
--- Jrafika/app/src/main/java/com/jrafika/jrafika/task/filters/customfilter/CustomFilterOptionFragment.kt
+++ Jrafika/app/src/main/java/com/jrafika/jrafika/task/filters/customfilter/CustomFilterOptionFragment.kt
@@ -2,34 +2,50 @@ package com.jrafika.jrafika.task.filters.customfilter
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.GridView
-import android.widget.RadioButton
-import android.widget.TextView
+import android.widget.*
 import com.jrafika.jrafika.R
 
 class CustomFilterOptionFragment: Fragment() {
 
-    var matrixGridView: GridView? = null
+    val SOBEL_HORIZONTAL_KERNEL = floatArrayOf(-1.0f, -2.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 2.0f, 1.0f)
+    val SOBEL_VERTICAL_KERNEL = floatArrayOf(-1.0f, 0.0f, 1.0f, -2.0f, 0.0f, 2.0f, -1.0f, 0.0f, 1.0f)
+    val PREWITT_KERNEL = floatArrayOf(1.0f, 0.0f, -1.0f, 1.0f, 0.0f, -1.0f, 1.0f, 0.0f, -1.0f)
+    val FREI_CHEN_KERNEL = floatArrayOf()
+
+    val textViewMatrix = ArrayList<EditText>()
+
+    var proceedFunction: ((FloatArray) -> Unit)? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.custom_filter_option_fragment_layout, container, false)
     }
 
+    fun setMatrix(kernel: FloatArray) {
+        for (i in 0..8) {
+            textViewMatrix.get(i).setText("" + kernel[i])
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        matrixGridView = view.findViewById(R.id.matrix_grid_view)
+        val matrixGridView = view.findViewById<GridView>(R.id.matrix_grid_view)
+        val proceedButton = view.findViewById<Button>(R.id.proceedButton)
 
-        val sobelHorizontalOption = view.findViewById<RadioButton>(R.id.horizontal_sobel_matrix)
-        val sobelVerticalOption = view.findViewById<RadioButton>(R.id.vertical_sobel_matrix)
+        val matrixTemplateOptions = view.findViewById<RadioGroup>(R.id.matrix_template)
 
-        var context = this.context
+        for (i in 0..8) {
+            val editText = EditText(this.context)
+            editText.inputType = InputType.TYPE_CLASS_NUMBER
+            editText.setText("0.11")
+            textViewMatrix.add(editText)
+        }
+
         matrixGridView!!.adapter = object: BaseAdapter() {
             override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-                val textView= TextView(context)
-                return textView
+                return textViewMatrix.get(position)
             }
             override fun getItem(position: Int): Any {
                 return 0
@@ -38,7 +54,35 @@ class CustomFilterOptionFragment: Fragment() {
                 return 0
             }
             override fun getCount(): Int {
-                return 9
+                return textViewMatrix.size
+            }
+        }
+
+        val kernelMap = HashMap<Int, FloatArray>()
+        kernelMap.put(R.string.sobel_horizontal, SOBEL_HORIZONTAL_KERNEL)
+        kernelMap.put(R.string.sobel_vertical, SOBEL_VERTICAL_KERNEL)
+        kernelMap.put(R.string.prewitt, PREWITT_KERNEL)
+
+        for (name in kernelMap.keys) {
+            val view = RadioButton(this.context)
+            view.setText(name)
+            matrixTemplateOptions.addView(view)
+            val kernel = kernelMap.get(name)
+            view.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked) {
+                    setMatrix(kernel!!)
+                }
+            }
+        }
+        matrixTemplateOptions.invalidate()
+
+        proceedButton.setOnClickListener {
+            val kernel = FloatArray(9)
+            for (i in 0..8) {
+                kernel[i] = textViewMatrix.get(i)!!.text.toString().toFloat()
+            }
+            if (proceedFunction != null) {
+                proceedFunction!!(kernel)
             }
         }
     }
